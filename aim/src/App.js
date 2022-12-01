670a02b88827b8912b1093907680e0cdd6dfd415
DispicableLee <lee.robert053@gmail.com>
2022-12-01 09:21:15 -0500

    initial react setup

---
+++ aim/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ChatSesh from './components/ChatShesh'
+
+function App() {
+  return (
+    <div className="App">
+      <ChatSesh/>
+    </div>
+  );
+}
+
+export default App;
