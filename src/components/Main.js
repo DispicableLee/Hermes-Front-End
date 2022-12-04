9b23cf1dc2b3526cc81439de06eaa6865f597c4a
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-12-03 21:56:38 -0500

    user signup and login

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,35 +1,36 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Login from './Login';
 import Home from './Home';
 import Chats from './Chats';
 import Contacts from './Contacts';
 import Profile from './Profile';
+import SignUp from "./SignUp";
 
 function Main({ isLoggedIn, setIsLoggedIn }) {
     const [convoData, setConvoData] = useState({});
     const [currentUser, setCurrentUser] = useState("");
     const navigate = useNavigate();
 
-    function logUserIn(userObj) {
-        fetch("http://localhost:3000/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(userObj)
-        })
-            .then(res => {
-                if (res.ok) {
-                    console.log(res);
-                    res.json().then(setCurrentUser(userObj.username));
-                    getConversations(userObj.username);
-                    setIsLoggedIn(true);
-                    navigate("/chats");
-                } else {
-                    res.json().then(e => console.log(e))
-                }
-            });
-    }
+    // function logUserIn(userObj) {
+    //     fetch("http://localhost:3000/login", {
+    //         method: "POST",
+    //         headers: { "Content-Type": "application/json", },
+    //         body: JSON.stringify(userObj)
+    //     })
+    //         .then(res => {
+    //             if (res.ok) {
+    //                 console.log(res);
+    //                 res.json().then(setCurrentUser(userObj.username));
+    //                 getConversations(userObj.username);
+    //                 setIsLoggedIn(true);
+    //                 navigate("/chats");
+    //             } else {
+    //                 res.json().then(e => console.log(e))
+    //             }
+    //         });
+    // }
 
     function getConversations(user) {
         fetch(`http://localhost:3000/myconversations?user=${user}`)
@@ -42,13 +43,25 @@ function Main({ isLoggedIn, setIsLoggedIn }) {
     }
 
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/chats" element={<Chats convoData={convoData} />} />
-            <Route path="/login" element={<Login logUserIn={logUserIn} />} />
-            <Route path="/contacts" element={<Contacts />} />
-            <Route path="/profile" element={<Profile />} />
-        </Routes>
+        <main>
+            {isLoggedIn ? (
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/chats" element={<Chats convoData={convoData} />} />
+                    {/* <Route path="/login" element={<Login logUserIn={logUserIn} />} /> */}
+                    <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+                    <Route path="/contacts" element={<Contacts />} />
+                    <Route path="/profile" element={<Profile />} />
+                </Routes>
+                ) : (
+                <Routes>
+                    <Route path="/signup" element={<SignUp setIsLoggedIn={setIsLoggedIn} />}/>
+                    <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />}/>
+                    <Route path="/" element={<Home />}/>
+                </Routes>
+                )
+            }
+        </main>
     )
 };
 
