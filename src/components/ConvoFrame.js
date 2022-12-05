6bccbeb8d77dde28b3c31e7dd82edba1bba393ec
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-12-05 11:15:38 -0500

    add send a message

--- src/components/ConvoFrame.js
+++ src/components/ConvoFrame.js
@@ -12,111 +12,109 @@ import { TextField } from "@mui/material";
 import MsgSent from "./MsgSent";
 import MsgReceived from "./MsgReceived";
 
-export default function ConvoFrame({ convoData, sendNewMessage }) {
+function ConvoFrame({ selectedChat, sendNewMessage }) {
   const [newMessage, setNewMessage] = useState("");
 
-  let messageList;
-  if (!!convoData.messages) {
-    messageList = convoData.messages;
-  } else {
-    return null;
-  }
-
+  
   function handleSubmit(e) {
     e.preventDefault();
     sendNewMessage(newMessage);
   }
-
+  
   function handleChange(e) {
     setNewMessage(e.target.value);
   }
-
-  return (
-    <div>
-      <Card
-        sx={{ maxWidth: "50%" }}
-        style={{
-          margin: "auto",
-        }}
-      >
-        <CardHeader
-          avatar={
-            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-              R
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title={"ConvoTitle: " + convoData.title}
-          subheader={
-            "Participants: " + convoData.users.map((u) => u.username).join(", ")
-          }
-        />
-        <CardMedia
-          component="img"
-          height="194"
-          image="/static/images/cards/paella.jpg"
-          alt="Paella dish"
-        />
-        <CardContent>
-          {messageList.map((msg, i) => {
-            let renderMessage;
-            if (msg.sender === "David") {
-              renderMessage = (
-                <aside
-                  key={i + 10}
-                  style={{
-                    // marginLeft: "20px",
-                    margin: "0 0 10px 10px",
-                    maxWidth: "50%",
-                    float: "right",
-                    position: 'static'
-                  }}
-                >
-                  <MsgSent key={`Convo_id: ${i + 1}`} msg={msg} />
-                </aside>
-              );
-            } else {
-              renderMessage = (
-                <aside
-                  key={i + 10}
-                  style={{
-                    marginLeft: "20px",
-                    maxWidth: "50%",
-                    float: "left",
-                    position: "static",
-                  }}
-                >
-                  <MsgReceived key={`Convo_id: ${i + 1}`} msg={msg} />
-                </aside>
-              );
+  
+  if (!!selectedChat) {
+    return (
+      <div>
+        <Card
+          sx={{ maxWidth: "50%" }}
+          style={{
+            margin: "auto",
+          }}
+        >
+          <CardHeader
+            avatar={
+              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                R
+              </Avatar>
             }
-            return <div>{renderMessage}</div>;
-          })}
-        </CardContent>
-        <form onSubmit={handleSubmit}>
-          <TextField
-            variant="outlined"
-            label="New Message"
-            style={{
-              width: "80%",
-            }}
-            value={newMessage}
-            onChange={handleChange}
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title={"ConvoTitle: " + selectedChat.title}
+            subheader={
+              "Participants: " + selectedChat.users.map((u) => u.username).join(", ")
+            }
           />
-          <Button
-            variant="contained"
-            color="success"
-            type="submit"
-            style={{ width: "16%", margin: "2%" }}
-          >
-            Send
-          </Button>
-        </form>
-      </Card>
-    </div>
-  );
+          {/* <CardMedia
+            component="img"
+            height="194"
+            image=""
+            alt=""
+          /> */}
+          <CardContent>
+            {selectedChat.messages.map((msg, i) => {
+              let renderMessage;
+              if (msg.sender === "David") {
+                renderMessage = (
+                  <aside
+                    // key={`message: ${selectedChat.id}`}
+                    style={{
+                      // marginLeft: "20px",
+                      margin: "0 0 10px 10px",
+                      maxWidth: "50%",
+                      float: "right",
+                      position: 'static'
+                    }}
+                  >
+                    <MsgSent key={msg.id} msg={msg} />
+                  </aside>
+                );
+              } else {
+                renderMessage = (
+                  <aside
+                    // key={i + 10}
+                    style={{
+                      marginLeft: "20px",
+                      maxWidth: "50%",
+                      float: "left",
+                      position: "static",
+                    }}
+                  >
+                    <MsgReceived key={`message: ${msg.id}`} msg={msg} />
+                  </aside>
+                );
+              }
+              return <div>{renderMessage}</div>;
+            })}
+          </CardContent>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              variant="outlined"
+              label="New Message"
+              style={{
+                width: "80%",
+              }}
+              value={newMessage}
+              onChange={handleChange}
+            />
+            <Button
+              variant="contained"
+              color="success"
+              type="submit"
+              style={{ width: "16%", margin: "2%" }}
+            >
+              Send
+            </Button>
+          </form>
+        </Card>
+      </div>
+    );
+  }
 }
+
+export default ConvoFrame;
