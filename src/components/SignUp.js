bfe9ef2c890ec4e3643742dd1058013e1e350ca1
Chris Li <chris.lty07@gmail.com>
2022-12-05 09:52:29 -0500

    Updated Signup Form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,59 +1,106 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { TextField } from "@mui/material";
+import Button from "@mui/material/Button"
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
 
 function SignUp({ setUser }) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [userData, setUserData] = useState({
+    username: "",
+    password: "",
+    passwordConfirm: "",
+    email: "",
+    avatar: ""
+  });
+  const navigate = useNavigate();
+
+  function handleChange(e) {
+    setUserData({ ...userData, [e.target.name]: e.target.value })
+    // console.log(userData)
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
-    fetch("http://localhost:3000/signup", {
+    fetch("/signup", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        username,
-        password,
-        password_confirmation: passwordConfirmation,
-      }),
+      body: JSON.stringify(userData),
     }).then((r) => {
       if (r.ok) {
-        r.json().then((user) => setUser(user));
+        r.json().then((user) => {
+          setUser(user);
+          setUserData({
+            username: "",
+            password: "",
+            passwordConfirm: "",
+            email: "",
+            avatar: ""
+          })
+          navigate("/chats");
+        });
       }
     });
   }
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <h1>Sign Up</h1>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          autoComplete="off"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          autoComplete="current-password"
-        />
-        <label htmlFor="password">Password Confirmation</label>
-        <input
-          type="password"
-          id="password_confirmation"
-          value={passwordConfirmation}
-          onChange={(e) => setPasswordConfirmation(e.target.value)}
-          autoComplete="current-password"
-        />
-        <button type="submit">Sign Up</button>
-      </form>
+      <Card>
+        <CardHeader title="Create an Account" />
+        <CardContent>
+          <form onSubmit={handleSubmit}
+            style={{
+              margin: "0 30% 0 30%",
+              maxWidth: "50%",
+            }}
+          >
+            <TextField
+              variant="outlined"
+              type="text"
+              label="Username"
+              autoComplete="off"
+              name="username"
+              value={userData.username}
+              onChange={(e) => handleChange(e)}
+            />
+            <br />
+            <TextField
+              variant="outlined"
+              type="text"
+              label="Email"
+              name="email"
+              autoComplete="off"
+              value={userData.email}
+              onChange={(e) => handleChange(e)}
+            />
+            <br />
+            <TextField
+              variant="outlined"
+              type="password"
+              name="password"
+              label="Password"
+              value={userData.password}
+              onChange={(e) => handleChange(e)}
+              autoComplete="current-password"
+            />
+            <br />
+            <TextField
+              variant="outlined"
+              type="password"
+              name="passwordConfirm"
+              label="Confirm Password"
+              value={userData.passwordConfirm}
+              onChange={(e) => handleChange(e)}
+              autoComplete="current-password"
+            />
+            <br />
+            <Button variant="contained" type="submit">Sign Up</Button>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
