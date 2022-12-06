826cbb1bb9dd93781cf990e3a8b56823488c8ef8
Chris Li <chris.lty07@gmail.com>
2022-12-06 11:20:14 -0500

    contacts

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,9 +1,25 @@
 import * as React from 'react';
-import {useState, useEffect} from 'react'
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
 
 function Contacts() {
+    React.useEffect(() => {
+        fetch("/mycontacts")
+            .then(r => r.json())
+            .then(console.log)
+    }, [])
+
     return (
-        <div></div>
+        <div>
+            <Card>
+                <CardHeader title="My Contacts" />
+                <CardContent>
+
+                </CardContent>
+
+            </Card>
+        </div>
     )
 }
 
