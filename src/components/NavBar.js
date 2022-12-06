18e39c00058235ee0b209fc26cb5b26a74c45c5b
DispicableLee <lee.robert053@gmail.com>
2022-12-06 11:16:47 -0500

    added stuff to drawer and got rid of navbar buttons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,170 +6,140 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import * as React from 'react';
+import * as React from "react";
 import { useState } from "react";
-import { styled, useTheme } from '@mui/material/styles';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiAppBar from '@mui/material/AppBar';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import { styled, useTheme } from "@mui/material/styles";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
 // import { Navigate } from "react-router-dom";
 import { Link, useNavigate } from "react-router-dom";
 //============== main styling ===========================================
 const drawerWidth = 240;
 
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
-    ({ theme, open }) => ({
-        flexGrow: 1,
-        padding: theme.spacing(3),
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        marginLeft: `-${drawerWidth}px`,
-        ...(open && {
-            transition: theme.transitions.create('margin', {
-                easing: theme.transitions.easing.easeOut,
-                duration: theme.transitions.duration.enteringScreen,
-            }),
-            marginLeft: 0,
-        }),
-    }),
-);
 //============================ drawer header styling =======================================
-const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
 }));
 
 function NavBar({ user, setUser }) {
-    //=========================== handle open/close====================================
-    const theme = useTheme();
-    const [open, setOpen] = useState(false);
+  //=========================== handle open/close====================================
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
 
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
 
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  function handleEditProfileClick() {
+    navigate("/profile");
+  }
 
-    function handleEditProfileClick() {
-        navigate("/profile")
-    }
+  function handleLogoutClick() {
+    fetch("/logout", { method: "DELETE" }).then((r) => {
+      if (r.ok) {
+        setUser(null);
+        navigate("/login");
+      }
+    });
+  }
 
-    function handleLogoutClick() {
-        fetch("/logout", { method: "DELETE" })
-            .then((r) => {
-                if (r.ok) {
-                    setUser(null);
-                    navigate("/login")
-                }
-            });
-    }
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" color="error">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={handleDrawerOpen}
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+              hermes
+            </Typography>
+            <div>
+              {user ? (
+                <>
+                  <Typography color="inherit" onClick={handleEditProfileClick}>
+                    Hi, {user.username}!
+                  </Typography>
+                </>
+              ) : (
+                <>
+                  <Link to="/signup">Signup</Link>
+                  <Link to="/login">Login</Link>
+                </>
+              )}
+            </div>
+          </Toolbar>
+        </AppBar>
 
-    return (
-        <div>
-            <Box sx={{ flexGrow: 1 }}>
-                <AppBar position="static" color="error">
-                    <Toolbar>
-                        <IconButton
-                            size="large"
-                            edge="start"
-                            color="inherit"
-                            aria-label="menu"
-                            onClick={handleDrawerOpen}
-                            sx={{ mr: 2 }}
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-                            hermes
-                        </Typography>
-                        <div>
-                            {user ? (
-                                <>
-                                    <Button color="inherit" onClick={handleEditProfileClick}>Hi, {user.username}! Profile</Button>
-                                    <Button color="inherit" onClick={() => navigate("/chats")}>Chats</Button>
-                                    <Button color="inherit" onClick={() => navigate("/contacts")}>Contacts</Button>
-                                    <Button color="inherit" onClick={handleLogoutClick}>Logout</Button>
-                                </>
-                            ) : (
-                                <>
-                                    <Link to="/signup">Signup</Link>
-                                    <Link to="/login">Login</Link>
-                                </>
-                            )}
-                        </div>
-                    </Toolbar>
-                </AppBar>
-
-
-                <Drawer
-                    sx={{
-                        width: drawerWidth,
-                        flexShrink: 0,
-                        '& .MuiDrawer-paper': {
-                            width: drawerWidth,
-                            boxSizing: 'border-box',
-                        },
-                    }}
-                    variant="persistent"
-                    anchor="left"
-                    open={open}
-                >
-                    <DrawerHeader>
-                        <IconButton onClick={handleDrawerClose}>
-                            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-                        </IconButton>
-                    </DrawerHeader>
-                    <Divider />
-                    <List>
-                        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                            <ListItem key={text} disablePadding>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                                    </ListItemIcon>
-                                    <ListItemText primary={text} />
-                                </ListItemButton>
-                            </ListItem>
-                        ))}
-                    </List>
-                    <Divider />
-                    <List>
-                        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                            <ListItem key={text} disablePadding>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                                    </ListItemIcon>
-                                    <ListItemText primary={text} />
-                                </ListItemButton>
-                            </ListItem>
-                        ))}
-                    </List>
-                </Drawer>
-            </Box>
-        </div>
-    )
+        <Drawer
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+          variant="persistent"
+          anchor="left"
+          open={open}
+        >
+          <DrawerHeader>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === "ltr" ? (
+                <ChevronLeftIcon />
+              ) : (
+                <ChevronRightIcon />
+              )}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+          <List>
+{/* ====================================== instantiating all the links ========================================= */}
+            <ListItemButton onClick={() => navigate("/profile")}>
+                Profile
+            </ListItemButton>
+            <ListItemButton onClick={() => navigate("/contacts")}>
+                Contacts
+            </ListItemButton>
+            <ListItemButton onClick={() => navigate("/chats")}>
+                Chats
+            </ListItemButton>
+            <ListItemButton onClick={handleLogoutClick}>
+                Logout
+            </ListItemButton>
+          </List>
+        </Drawer>
+      </Box>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
