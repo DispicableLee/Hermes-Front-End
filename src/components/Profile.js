76a4a0967f22adff68dba9e0a02ada9a9bbc8c21
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-12-06 21:12:10 -0500

    fixed: message persist and onClick edit bug. to-do: add patch request to edit message

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
